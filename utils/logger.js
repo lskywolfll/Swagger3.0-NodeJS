246ac520ed2ce164e3f071411f7417f2c826071a
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-06-04 19:47:04 -0400

    se eliminan logs residuales y se ordena el codigo

--- utils/logger.js
+++ utils/logger.js
@@ -1,25 +1,24 @@
 const {
-createLogger,format,transports
-
-} = require ('winston')
-
-
+    createLogger,
+    format,
+    transports
+} = require('winston')
 
 module.exports = createLogger({
-    format : format.combine(format.simple()),
+    format: format.combine(format.simple()),
     transports: [
         new transports.Console({
             level: 'debug'
-            
+
         }),
         new transports.File({
-            maxsize : 5120000,
+            maxsize: 5120000,
             maxFiles: 5,
-            filename:__dirname +'/logs/log-api.log',
-            
-        }),
-       
-    ]
-}) 
+            filename: __dirname + '/logs/log-api.log',
 
-  
+        }),
+
+    ]
+})
+
+
